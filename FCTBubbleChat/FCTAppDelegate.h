a3bd21fea2cbcb9465d7ca18cefe3fde147baa66
SebastienFCT <sebastienfct@gmail.com>
2014-07-01 10:55:28 +0400

    init

---
+++ FCTBubbleChat/FCTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FCTAppDelegate.h
+//  FCTBubbleChat
+//
+//  Created by Sebastien FCT on 7/1/14.
+//  Copyright (c) 2014 Sebastien FCT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FCTBubbleChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FCTBubbleChat
+//
+//  Created by Sebastien FCT on 7/1/14.
+//  Copyright (c) 2014 Sebastien FCT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FCTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FCTAppDelegate class]));
+    }
+}
