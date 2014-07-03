94fb414a572e7a9a258391a0e4ee43be8993877e
SebastienFCT <sebastienfct@gmail.com>
2014-07-03 14:20:39 +0400

    Custom tableViewCell step1

--- FCTBubbleChat/FCTBubbleTableViewCell.m
+++ FCTBubbleChat/FCTBubbleTableViewCell.m
@@ -29,9 +29,26 @@
 - (void)makeTheCell
 {
     [self.customView removeFromSuperview];
+    
     UILabel *test = [[UILabel alloc] initWithFrame:CGRectMake(40, 5, 100, 50)];
     test.text = @"blablablablabla";
-    self.customView = test;
+    
+    UIFont *font = [UIFont fontWithName:@"Arial" size:16];
+    NSAttributedString *attributedText = [[NSAttributedString alloc] initWithString:(test.text ? test.text : @"") attributes:@{NSFontAttributeName: font}];
+    CGRect rect = [attributedText boundingRectWithSize:(CGSize){220, 10000} options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+    CGSize size = rect.size;
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    label.numberOfLines = 0;
+    label.lineBreakMode = NSLineBreakByWordWrapping;
+    label.text = (test.text ? test.text : @"");
+    label.font = font;
+    label.backgroundColor = [UIColor clearColor];
+    [label.layer setCornerRadius:8.0f];
+    [label.layer setBorderWidth:1.0];
+    [label.layer setBorderColor:[UIColor lightGrayColor].CGColor];
+
+    self.customView = label;
     
     [self.contentView addSubview:self.customView];
 }
