c08e4c8540e8518e40fef812f17143f8d3406bbf
SebastienFCT <sebastienfct@gmail.com>
2016-02-11 12:15:13 -0600

    swift branch added

---
+++ SWIFT/FCTBubbleChat/FCTBubbleChat/FCTBubbleChat.h
@@ -0,0 +1,19 @@
+//
+//  FCTBubbleChat.h
+//  FCTBubbleChat
+//
+//  Created by sebastien FOCK CHOW THO on 2016-02-11.
+//  Copyright © 2016 sebfct. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FCTBubbleChat.
+FOUNDATION_EXPORT double FCTBubbleChatVersionNumber;
+
+//! Project version string for FCTBubbleChat.
+FOUNDATION_EXPORT const unsigned char FCTBubbleChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCTBubbleChat/PublicHeader.h>
+
+
